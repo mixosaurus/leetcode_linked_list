ed47f47df41df8cbc7f15b8e923fd57e6be69f35
snake <lcpdfk@163.com>
2020-07-04 12:00:18 +0800

    删除无用代码

---
+++ Linked_List_Cycle/solution.h
@@ -0,0 +1,12 @@
+#pragma once
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode() :val(0),next(nullptr){}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head);
+};
