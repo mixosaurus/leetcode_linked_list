9b9a7d985ee0c467b8d88cf167af66649b9121b4
snake <lcpdfk@163.com>
2020-07-07 11:01:56 +0800

    203. 移除链表元素

---
+++ Remove_Linked_List_Elements/solution.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stack> 
+#include "solution.h"
+
+using namespace std;
+
+ListNode* Solution::removeElements(ListNode* head, int val) {
+    return nullptr;
+}
---
+++ Remove_Linked_List_Elements/solution.h
@@ -0,0 +1,13 @@
+#pragma once
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* Solution::removeElements(ListNode* head, int val);
+};
