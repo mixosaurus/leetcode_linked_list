e68c8145d096910daf6f97c094d76e3c559ddbbd
snake <lcpdfk@163.com>
2023-08-16 15:53:00 +0800

    2. 两数相加

--- Add_two_numbers/solution.h
+++ Add_two_numbers/solution.h
@@ -1,4 +1,11 @@
 #pragma once
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode() :val(0), next(nullptr) {}
+};
+
 class Solution {
 
 };
